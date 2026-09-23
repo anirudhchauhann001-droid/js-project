838c89431bf87694051111ed8308d6385073d9d3
Anirudh chauhan <anirudhchauhann001@gmail.com>
2026-09-23 19:04:34 +0530

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
     quantity: 2
 }, {
@@ -23,4 +23,16 @@ export function addToCart(productId){
             quantity : 1
         });
     }
+}
+
+export function removeFromCart(productId){
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }
